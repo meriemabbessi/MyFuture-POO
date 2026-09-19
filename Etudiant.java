5a2828ab937369b53f7aa5154b9a9e5cdfe43efe
Meriem Abbessi <meriem.abbessi@edu.isetcom.tn>
2026-09-19 17:09:38 +0200

    Ajout de la classe Etudiant

---
+++ Etudiant.java
@@ -0,0 +1,15 @@
+public class Etudiant {
+    public static void main(String[] args) {
+
+        System.out.println("==============================");
+        System.out.println("      INFORMATIONS ETUDIANT");
+        System.out.println("==============================");
+
+        System.out.println("Nom : Mariem");
+        System.out.println("Classe : GTIC L1");
+        System.out.println("Langage : Java");
+
+        System.out.println("==============================");
+    }
+
+}
